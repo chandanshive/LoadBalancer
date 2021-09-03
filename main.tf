280870b2230b02474df256a2fb630a38b264710a
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-09-03 11:50:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
 resource "aws_lb" "test" {
-  name               = "Load_Balancer"
+  name               = "LoadBalancer"
   internal           = false
   load_balancer_type = "application"
 
-  enable_deletion_protection = true
-
-  access_logs {
-    prefix  = "test-lb"
-    enabled = true
-  }
+  enable_deletion_protection = false
 
   tags = {
     Environment = "production"
